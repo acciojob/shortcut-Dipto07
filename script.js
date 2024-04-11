005c0a377acc17eecabe041b8543d63a845babdc
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-11 07:20:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1.length === 0 || s2.length === 0) {
+    return '';
+  }
+
+  const initialLetters = s1[0] + s2[0];
+
+  return initialLetters;
 }
 
 // Do not change the code below.
